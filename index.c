35c37895e115a862d7414e5207d431f2319124eb
cristichiu <grajdiancristian@gmail.com>
2024-11-01 14:22:14 +0200

    update

--- index.c
+++ index.c
@@ -3,49 +3,65 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-typedef struct {
+typedef struct settings {
+    int unique;
+    int alloc;
+    char *type;
+} settings;
+
+typedef struct model_data {
+    char *name;
+    settings set;
+} model_data;
+
+typedef struct model {
     char *name;
     int nums;
-    char **data;
+    model_data *data;
 } model;
 
-int main() {
+void display(model *mod) {
+    printf("%s\n", mod->name);
+    for(int j=0; j<mod->nums; j++) {
+        printf("%s", mod->data[j]);
+    }
+}
+
+model *init(char *path) {
     FILE *file;
-    file = fopen("./migration/user.chiu", "r");
+    file = fopen(path, "r");
     char *line = (char*)malloc(256);
     int started = 0;
-    model models[100];
-    int modelsIndex = 0;
+    model *mod = (model*)malloc(sizeof(model));
     int modelIndex = 0;
     while(fgets(line, 256, file)) {
         if(strstr(line, "END") == line) {
             started = 0;
-            modelsIndex++;
-            modelIndex = 0;
         }
         if(started) {
             while(isspace(*line)) *line++;
-            printf("%s", line);
-            strcpy(models[modelsIndex].data[modelIndex++], line);
+            char *token = strtok(line, ":");
+            mod->data[modelIndex++].name = (char*)malloc(strlen(token) * sizeof(char) + 1);
+            strcpy(mod->data[modelIndex++].name, token);
         }
         if(strstr(line, "START") == line) {
             char *token = strtok(line, " ");
-            model cMolde = models[modelsIndex];
-            token = strtok(NULL, " "); cMolde.name = token;
-            token = strtok(NULL, " "); cMolde.nums = atoi(token);
-            cMolde.data = (char**)malloc(cMolde.nums * 256);
-            for(int i=0; i<cMolde.nums; i++) cMolde.data[i] = (char*)malloc(256);
+            token = strtok(NULL, " ");
+            mod->name = (char*)malloc(strlen(token)*sizeof(char) + 1);
+            strcpy(mod->name, token);
+            token = strtok(NULL, " ");
+            mod->nums = atoi(token);
+            mod->data = (model_data*)malloc(mod->nums * sizeof(model_data*));
             started = 1;
         }
     }
     fclose(file);
-    printf("%d", modelsIndex);
-    for(int i=0; i<modelsIndex; i++) {
-        printf("Modelul %s\n", models[i].name);
-        for(int j=0; j<models[i].nums; j++) {
-            printf("%d", models[i].data[j]);
-        }
-    }
+    return mod;
+}
 
+int main() {
+    model *user = init("./migration/user.chiu");
+    display(user);
+    // find(user, "name==test", "name,age")
     return 0;
 }
